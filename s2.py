b6ce701c522bbbf0a16d93bae8f9a85b386e605b
junsu <vfv0121@gmail.com>
2016-05-10 19:29:25 +0900

    hi

---
+++ s2.py
@@ -0,0 +1,38 @@
+l1 = 'abcdefg'
+l2 = range(5)
+
+print l1[:4]
+print l2[1:-1]
+
+
+for i in l1:
+    print i
+
+for kk, num in enumerate(l1):
+    print kk, num
+
+l  = range(20,25)
+print l
+
+## list comprehensions
+##print [x for x in range(1,5)]
+
+
+## dictionary
+## key, value
+
+users = {}
+l_t1 = [1,2,3]
+l_t2 = [1,2,3]
+users['j'] = '010101010'
+users[l_t1] = '111'
+
+print users[l_t2]
+
+
+## assignment 2
+
+
+
+
+
